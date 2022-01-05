025a3e4fc1831bfdfe07e23dc1520f730a028346
jacikot <jana.toljaga725@yahoo.com>
2022-01-05 15:12:07 +0100

    on click listener added to fields

--- app/src/main/java/rs/ac/bg/etf/monopoly/MainActivity.java
+++ app/src/main/java/rs/ac/bg/etf/monopoly/MainActivity.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import rs.ac.bg.etf.monopoly.databinding.ActivityMainBinding;
+import rs.ac.bg.etf.monopoly.db.DBMonopoly;
+import rs.ac.bg.etf.monopoly.db.Property;
+import rs.ac.bg.etf.monopoly.db.Repository;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding amb;
+
+    private DBMonopoly database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         amb=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(amb.getRoot());
+        database=DBMonopoly.getInstance(this);
+        Repository repo=new Repository(this,database.getDao());
+        repo.getProperty(0).observe(this,e->{
+            if(e==null){
+                repo.initDB();
+            }
+        });
     }
 }
--- app/src/main/java/rs/ac/bg/etf/monopoly/TableFragment.java
+++ app/src/main/java/rs/ac/bg/etf/monopoly/TableFragment.java
@@ -1,5 +1,6 @@
 package rs.ac.bg.etf.monopoly;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,11 +8,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import rs.ac.bg.etf.monopoly.databinding.FragmentTableBinding;
 
@@ -35,12 +38,25 @@ public class TableFragment extends Fragment {
         model= new ViewModelProvider(activity).get(GameModel.class);
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         amb=FragmentTableBinding.inflate(inflater,container,false);
-
+        TypedArray images=getResources().obtainTypedArray(R.array.ids);
+        for(int i=0;i<images.length();i++){
+            int id=images.getResourceId(i,0);
+            int index=i;
+            int start=R.id.start;
+            amb.getRoot().findViewById(id).setOnClickListener(e->{
+                TableFragmentDirections.Details action=TableFragmentDirections.details();
+                action.setIndex(index);
+                controller.navigate(action);
+            });
+        }
+        images.recycle();
         return amb.getRoot();
     }
 
@@ -48,5 +64,6 @@ public class TableFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         controller= Navigation.findNavController(view);
+
     }
 }
